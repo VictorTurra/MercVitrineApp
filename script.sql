890e7d687dcfe67d0e844678c9889d001be46099
Victor Turra <turra667@gmail.com>
2019-11-12 21:19:10 -0200

    Atualizado Script MySql do banco

--- script.sql
+++ script.sql
@@ -1,3 +1,4 @@
+DROP DATABASE MercVitrineApp;
 CREATE DATABASE MercVitrineApp;
 USE MercVitrineApp;
 
@@ -6,102 +7,139 @@ CREATE TABLE IF NOT EXISTS Produto (
   Descricao VARCHAR(45) NOT NULL,
   PrecoUN DECIMAL(6,2) NOT NULL,
   QtdEstoque INT NOT NULL,
-  PRIMARY KEY (idProduto));
+  PRIMARY KEY (idProduto),
+  CONSTRAINT check_produto CHECK ( QtdEstoque >= 0 AND PrecoUN > 0 )
+);
+
+CREATE TABLE IF NOT EXISTS Item (
+  idItem INT(11) NOT NULL AUTO_INCREMENT,
+  QtdItem INT(11) NOT NULL DEFAULT '1',
+  idProduto INT(11) NOT NULL,
+  PRIMARY KEY ( idItem, idProduto ),
+  CONSTRAINT fk_produto FOREIGN KEY ( idProduto )
+    REFERENCES Produto (idProduto),
+  CONSTRAINT check_item CHECK ( QtdItem > 0 )
+);
+
+CREATE TABLE IF NOT EXISTS Nivel (
+  idNivel INT(11) NOT NULL AUTO_INCREMENT,
+  Descricao VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idNivel)
+);
 
 CREATE TABLE IF NOT EXISTS Login (
   idLogin INT NOT NULL AUTO_INCREMENT,
   Email VARCHAR(45) NOT NULL,
   Username VARCHAR(45) NOT NULL,
   Senha CHAR(60) NOT NULL,
-  PRIMARY KEY (idLogin));
+  idPessoa INT NOT NULL,
+  idNivel INT NOT NULL,
+  PRIMARY KEY (idLogin),
+  CONSTRAINT fkLoginPessoa FOREIGN KEY ( idPessoa ) references Pessoa( idPessoa ),
+  CONSTRAINT fkLoginNivel FOREIGN KEY ( idNivel ) references Nivel( idNivel )
+);
 
-CREATE TABLE IF NOT EXISTS Cliente (
-  idCliente INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Pessoa (
+  idPessoa INT NOT NULL AUTO_INCREMENT,
   Nome VARCHAR(45) NOT NULL,
   Sobrenome VARCHAR(45) NOT NULL,
   CPF VARCHAR(45) NOT NULL,
   Telefone VARCHAR(45) NULL,
   idLogin INT NOT NULL,
-  PRIMARY KEY (idCliente),
-  CONSTRAINT fk_login
-    FOREIGN KEY (idLogin)
-    REFERENCES Login (idLogin));
+  PRIMARY KEY (idPessoa)
+);
+
 
 CREATE TABLE IF NOT EXISTS Feedback (
-  idCliente INT NOT NULL,
+  idPessoa INT NOT NULL,
   idFeedback INT NOT NULL,
   Descricao VARCHAR(100) NOT NULL,
   Avaliacao TINYINT NOT NULL,
   Likes INT NULL DEFAULT 0,
-  PRIMARY KEY (idFeedback, idCliente),
-  CONSTRAINT fk_cliente
-    FOREIGN KEY (idCliente)
-    REFERENCES Cliente (idCliente));
+  PRIMARY KEY (idFeedback, idPessoa),
+  CONSTRAINT fk_feedback_pessoa
+    FOREIGN KEY (idPessoa)
+    REFERENCES Pessoa (idPessoa),
+  CONSTRAINT check_feedback CHECK ( Likes > 0 AND Avaliacao BETWEEN 0 AND 5 )
+);
 
 
 CREATE TABLE IF NOT EXISTS Carrinho (
-  idCliente INT NOT NULL,
+  idPessoa INT NOT NULL,
   idItem INT NOT NULL,
   QtdItem INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (idCliente, idItem),
-  CONSTRAINT fk_cliente
-    FOREIGN KEY (idCliente)
-    REFERENCES Cliente (idCliente),
+  PRIMARY KEY (idPessoa, idItem),
+  CONSTRAINT fk_idPessoa
+    FOREIGN KEY (idPessoa)
+    REFERENCES Pessoa(idPessoa),
   CONSTRAINT fk_produto
     FOREIGN KEY (idItem)
-    REFERENCES Produto (idProduto));
+    REFERENCES Item(idItem),
+  CONSTRAINT check_carrinho CHECK ( QtdItem > 0 )
+);
 
 CREATE TABLE IF NOT EXISTS Endereco (
-  IdCliente INT NOT NULL,
+  idPessoa INT NOT NULL,
   idEndereco INT NOT NULL,
   Tipo VARCHAR(10) NOT NULL,
   Logradouro VARCHAR(45) NOT NULL,
   Numero INT NOT NULL,
   Complemento VARCHAR(45) NULL,
   Bairro VARCHAR(45) NOT NULL,
-  PRIMARY KEY (IdCliente, idEndereco),
-  CONSTRAINT fk_cliente
-    FOREIGN KEY (IdCliente)
-    REFERENCES Cliente (idCliente));
+  PRIMARY KEY (idPessoa, idEndereco),
+  CONSTRAINT fk_pessoa
+    FOREIGN KEY (idPessoa)
+    REFERENCES Pessoa(idPessoa)
+);
+
+CREATE TABLE IF NOT EXISTS TipoCartao (
+  idtipo_cartao INT NOT NULL auto_increment,
+  descricao VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idtipo_cartao)
+);
+
 
 CREATE TABLE IF NOT EXISTS Cartao (
-  IdCliente INT NOT NULL,
+  idPessoa INT NOT NULL,
   idCartao INT NOT NULL,
   Numero VARCHAR(45) NOT NULL,
-  Validade VARCHAR(45) NOT NULL,
+  validade_mes INT NOT NULL,
+  validade_ano INT NOT NULL,
   Titular VARCHAR(60) NOT NULL,
   CodVerificador CHAR(3) NOT NULL,
-  Tipo VARCHAR(45) NOT NULL,
-  PRIMARY KEY (IdCliente, idCartao),
-  CONSTRAINT fk_cartao_cliente
-    FOREIGN KEY (IdCliente)
-    REFERENCES Cliente (idCliente));
+  idTipoCartao VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idPessoa, idCartao),
+  CONSTRAINT fk_CartaoPessoa
+    FOREIGN KEY (idPessoa)
+    REFERENCES Pessoa (idPessoa),
+  CONSTRAINT fk_CartaoPessoa
+    FOREIGN KEY (idTipoCartao)
+    REFERENCES TipoCartao (idtipo_cartao));
 
 CREATE TABLE IF NOT EXISTS Pagamento (
   idPagamento INT NOT NULL AUTO_INCREMENT,
   Forma VARCHAR(45) NOT NULL,
-  idCliente INT NOT NULL,
+  idPessoa INT NOT NULL,
   idCartao INT NULL,
   Subtotal DECIMAL(6,2) NOT NULL,
   Status INT NOT NULL DEFAULT 0,
   PRIMARY KEY (idPagamento),
   CONSTRAINT fk_cartao
-    FOREIGN KEY (idCliente , idCartao)
-    REFERENCES Cartao (IdCliente , idCartao),
-  CONSTRAINT fk_cliente
-    FOREIGN KEY (idCliente)
-    REFERENCES Cliente (idCliente));
+    FOREIGN KEY (idPessoa , idCartao)
+    REFERENCES Cartao( idPessoa , idCartao )
+);
+
 
 CREATE TABLE IF NOT EXISTS Entrega (
   idEntrega INT NOT NULL AUTO_INCREMENT,
-  idCliente INT NOT NULL,
+  idPessoa INT NOT NULL,
   idEndereco INT NOT NULL,
   idPagamento INT NOT NULL,
   Status INT NOT NULL,
-  PRIMARY KEY (idEntrega, idCliente, idEndereco, idPagamento),
+  PRIMARY KEY (idEntrega),
   CONSTRAINT fk_endereco
-    FOREIGN KEY (idEndereco , idCliente)
-    REFERENCES Endereco (idEndereco , IdCliente),
+    FOREIGN KEY (idEndereco , idPessoa)
+    REFERENCES Endereco (idEndereco , idPessoa),
   CONSTRAINT fk_pagamento
     FOREIGN KEY (idPagamento)
     REFERENCES Pagamento (idPagamento));
